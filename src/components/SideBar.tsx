4cd2d001acfeff2631331a72f0b6b93efa4308c9
Braulio F. Lima <braulio.freires.lima@gmail.com>
2021-03-15 11:18:48 -0300

    chore:side bar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: GenreResponseProps['id'];
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }:SideBarProps) {
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              id={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  );
 }
